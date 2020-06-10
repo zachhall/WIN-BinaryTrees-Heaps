106d24ac2021e2a2392e4b1f51fea61e8d72b5db
Zach Hall <zachhallnc@gmail.com>
2020-06-10 10:41:08 -0400

    Add BinaryTree class built in class

---
+++ BinaryTree.java
@@ -0,0 +1,71 @@
+public class BinaryTree {
+    Node left, right;
+    int data;
+
+    public void Node(int data) {
+        this.data = data;
+    }
+
+    public void insert(int value) {
+        if (value <= data) {
+            if (left == null) {
+                left = new Node(value);
+            } else {
+                left.insert(value); // recursively continuing down the tree
+            }
+        } else {
+            if (right == null) {
+                right = new Node(value);
+            } else {
+                right.insert(value);
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (value == data) {
+            return true;
+        } else if (value < data) {
+            if (left == null) {
+                return false;
+            } else {
+                return left.contains(value);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.contains(value);
+            }
+        }
+    }
+
+    public void printInOrder() {
+        if (left != null) {
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if (right != null) {
+            right.printInOrder();
+        }
+    }
+
+    private BinaryTree createBinaryTree() {
+        BinaryTree bt = new BinaryTree();
+
+        bt.insert(50);
+        bt.insert(84);
+        bt.insert(24);
+        bt.insert(52);
+        bt.insert(12);
+        bt.insert(64);
+        bt.insert(99);
+        bt.insert(1);
+
+        return bt;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
